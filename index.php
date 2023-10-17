b324085b8be2ce37739d25e1a65f316da0e36872
jiajiechen2003 <jchen2223@politecnics.barcelona>
2023-10-17 18:46:40 +0200

    update1

--- index.php
+++ index.php
@@ -6,17 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>One Piece</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
 </head>
 
-<body style="height: 2000px">
+<body style="height: 3000px">
     <header>
         <nav class="navbar fixed-top navbar-expand-lg navbar-light ps-2 pt-4 pb-4">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#"><img src="img/logo.png" id="logo"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
@@ -38,16 +42,139 @@
 
     <div class="container-md mt-4">
         <div class="row">
+            <div class="col-xl-3 col-md-5">
+                <div class="card" id="carta">
+                    <div class="leader-card-stats">
+                        <div class="leader-power">
+                            <h3>5000</h3>
+                            <img src="img/rangedLogo.PNG" id="ranged">
+                        </div>
+                    </div>
+                    <div class="leader-img">
+                        <img src="img/luffyLeader.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-5">
+                <div class="card" id="carta">
+                    <div class="leader-card-stats">
+                        <div class="leader-power">
+                            <h3>5000</h3>
+                            <img src="img/slashLogo.PNG" id="slash">
+                        </div>
+                    </div>
+                    <div class="leader-img">
+                        <img src="img/zoroLeader.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-5">
+                <div class="card" id="carta">
+                    <div class="leader-card-stats">
+                        <div class="leader-power">
+                            <h3>5000</h3>
+                            <img src="img/strikeLogo.PNG" id="slash">
+                        </div>
+                    </div>
+                    <div class="leader-img">
+                        <img src="img/sanjiLeader.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-5">
+                <div class="card" id="carta">
+                    <div class="leader-card-stats">
+                        <div class="leader-power">
+                            <h3>5000</h3>
+                            <img src="img/slashLogo.PNG" id="slash">
+                        </div>
+                    </div>
+                    <div class="leader-img">
+                        <img src="img/lawLeader.png">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-md mt-4">
+        <div class="row">
+            <div class="col-xl-3 col-md-5">
+                <div class="card" id="carta">
+                    <div class="leader-card-stats">
+                        <div class="leader-power">
+                            <h3>5000</h3>
+                            <img src="img/rangedLogo.PNG" id="ranged">
+                        </div>
+                    </div>
+                    <div class="leader-img">
+                        <img src="img/hancock.webp">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-5">
+                <div class="card" id="carta">
+                    <div class="leader-card-stats">
+                        <div class="leader-power">
+                            <h3>5000</h3>
+                            <img src="img/slashLogo.PNG" id="slash">
+                        </div>
+                    </div>
+                    <div class="leader-img">
+                        <img src="img/nicoRobin.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-5">
+                <div class="card" id="carta">
+                    <div class="leader-card-stats">
+                        <div class="leader-power">
+                            <h3>5000</h3>
+                            <img src="img/strikeLogo.PNG" id="slash">
+                        </div>
+                    </div>
+                    <div class="leader-img">
+                        <img src="img/namiSwan.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-5">
+                <div class="card" id="carta">
+                    <div class="leader-card-stats">
+                        <div class="leader-power">
+                            <h3>5000</h3>
+                            <img src="img/slashLogo.PNG" id="slash">
+                        </div>
+                    </div>
+                    <div class="leader-img">
+                        <img src="img/bonney.jpeg">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-md mt-4">
+        <div class="row">
             <div class="col-lg-3">
                 <div class="card" id="carta">
                     <div class="card-body">
-                        
+
                     </div>
                 </div>
             </div>
             <div class="col-lg-3">
                 <div class="card" id="carta">
                     <div class="card-body">
+
                     </div>
                 </div>
             </div>
@@ -92,79 +219,11 @@
                     </div>
                 </div>
             </div>
-            
+
             <div class="col-lg-3">
                 <div class="card" id="carta">
                     <div class="card-body">
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-md mt-4">
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="card" id="carta">
-                    <div class="card-body">
-
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card" id="carta">
-                    <div class="card-body">
-
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card" id="carta">
-                    <div class="card-body">
-
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col-lg-3">
-                <div class="card" id="carta">
-                    <div class="card-body">
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-md mt-4">
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="card" id="carta">
-                    <div class="card-body">
-
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card" id="carta">
-                    <div class="card-body">
-
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3">
-                <div class="card" id="carta">
-                    <div class="card-body">
-
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col-lg-3">
-                <div class="card" id="carta">
-                    <div class="card-body">
-                        
                     </div>
                 </div>
             </div>
