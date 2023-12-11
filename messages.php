9b49452ae0bb04db17b3ba8d5567e5d61031bd34
jiajiechen2003 <jchen2223@politecnics.barcelona>
2023-12-11 17:04:28 +0100

    botones alert

--- messages.php
+++ messages.php
@@ -1,11 +1,11 @@
 <?php if (isset($_SESSION['addedCard'])) { ?>
 
-    <div class="alert alert-success" role="alert">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
         <?php
-            echo $_SESSION['addedCard'];
-            unset($_SESSION['addedCard']);
+        echo $_SESSION['addedCard'];
+        unset($_SESSION['addedCard']);
         ?>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
@@ -13,65 +13,65 @@
 
 <?php if (isset($_SESSION['addedGroup'])) { ?>
 
-<div class="alert alert-success" role="alert">
-    <?php
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <?php
         echo $_SESSION['addedGroup'];
         unset($_SESSION['addedGroup']);
-    ?>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
+        ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 <?php } ?>
 
 <?php if (isset($_SESSION['existingGroup'])) { ?>
 
-<div class="alert alert-danger" role="alert">
-    <?php
+    <div class="alert alert-danger" role="alert">
+        <?php
         echo $_SESSION['existingGroup'];
         unset($_SESSION['existingGroup']);
-    ?>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
+        ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 <?php } ?>
 
 <?php if (isset($_SESSION['deleted'])) { ?>
 
-<div class="alert alert-success" role="alert">
-    <?php
+    <div class="alert alert-success" role="alert">
+        <?php
         echo $_SESSION['deleted'];
         unset($_SESSION['deleted']);
-    ?>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
+        ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 <?php } ?>
 
 <?php if (isset($_SESSION['edited'])) { ?>
 
-<div class="alert alert-success" role="alert">
-    <?php
+    <div class="alert alert-success" role="alert">
+        <?php
         echo $_SESSION['edited'];
         unset($_SESSION['edited']);
-    ?>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
+        ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 <?php } ?>
 
 <?php if (isset($_SESSION['error'])) { ?>
 
-<div class="alert alert-danger" role="alert">
-    <?php
+    <div class="alert alert-danger" role="alert">
+        <?php
         echo $_SESSION['error'];
         unset($_SESSION['error']);
-    ?>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
+        ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 <?php } ?>
